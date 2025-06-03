6c91bd161a2bb2c13b457c975cb1b0c2edfc417d
steve <steve3816811205@gmail.com>
2025-06-03 15:14:51 +0800

    用springboot initializer 起專案

---
+++ src/main/java/com/steve/personal_note/PersonalNoteApplication.java
@@ -0,0 +1,13 @@
+package com.steve.personal_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steve/personal_note/PersonalNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.personal_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
